8c79ba07b757901bc40e5c79a00c1522176c5b80
Gabriel Aldiney <gabrielaldiney@gmail.com>
2025-01-25 08:08:20 -0300

    Depoloy ready

--- src/main/java/com/gabrieldev/dslist/dto/GameMinDTO.java
+++ src/main/java/com/gabrieldev/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.gabrieldev.dslist.dto;
 
 import com.gabrieldev.dslist.entities.Game;
+import com.gabrieldev.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -20,6 +21,15 @@ public class GameMinDTO {
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
     }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
+    }
+
 
     public Long getId() {
         return id;
--- src/main/java/com/gabrieldev/dslist/services/GameService.java
+++ src/main/java/com/gabrieldev/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.gabrieldev.dslist.services;
 import com.gabrieldev.dslist.dto.GameDTO;
 import com.gabrieldev.dslist.dto.GameMinDTO;
 import com.gabrieldev.dslist.entities.Game;
+import com.gabrieldev.dslist.projections.GameMinProjection;
 import com.gabrieldev.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,11 @@ public class GameService {
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x-> new GameMinDTO(x)).toList();
+    }
 
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x-> new GameMinDTO(x)).toList();
     }
 }
